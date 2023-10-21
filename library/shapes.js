c628aeb13dd7c2c590c49cf37c1ae7d9b100dd1e
moxnix <andrewchall92@gmail.com>
2023-10-21 02:17:04 -0700

    I got the shape elements rendering to the logo.svg page

--- library/shapes.js
+++ library/shapes.js
@@ -18,7 +18,7 @@ class Shape {
              <text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
              </svg>`;
              
-             fs.writeFile('./assets/circle.svg', circle, (err) => {
+             fs.writeFile('./assets/logo.svg', circle, (err) => {
                 if (err) throw err;
                 console.log('circle selected');
              });
@@ -26,9 +26,30 @@ class Shape {
         } else if(this.shape === 'Square') {
             // append to square.svg file
             console.log(this.shape);
+            const square = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+             <rect x="50" y="20" width="150" height="150" fill="${this.shapeColor}" />
+             <text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
+             </svg>`;
+             
+             fs.writeFile('./assets/logo.svg', square, (err) => {
+                if (err) throw err;
+                console.log('square selected');
+             });
+
         } else if(this.shape === 'Triangle') {
             // append to triangle.svg file
             console.log(this.shape);
+            // this isn't working?
+            const triangle = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 100 100">
+             <polygon points="350 100, 400 200, 300 200" fill="${this.shapeColor}"/>
+             <text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
+             </svg>`;
+             
+             fs.writeFile('./assets/logo.svg', triangle, (err) => {
+                if (err) throw err;
+                console.log('triangle selected');
+             });
+
         } else {
             return;
         };
